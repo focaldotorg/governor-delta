0f83059f1948411ec668810537aad1e1cb916502
deoidh <samuel.jj.gosling@gmail.com>
2026-06-23 10:42:33 +0100

    introduce mock erc20

--- test/GovernorDelta.t.sol
+++ test/GovernorDelta.t.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.13;
 
 import { Test, console } from "forge-std/Test.sol";
 import { GovernorDelta } from "@root/GovernorDelta.sol";
-import { IERC20 } from "@openzeppelin/token/ERC20/IERC20.sol";
+import { TestERC20 } from "./mock/TestERC20.sol";
 
 contract GovernorDeltaTest is Test {
 
-    IERC20 governorToken;
     GovernorDelta delta;
+    TestERC20 governorToken;
 
     function setUp() public {}
 
---
+++ test/mock/TestERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Test, console } from "forge-std/Test.sol";
+import { GovernorDelta } from "@root/GovernorDelta.sol";
+import { ERC20 } from "@openzeppelin/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+
+    constructor() ERC20("", "SHARE") {}
+
+    function mint(address to, uint amount) public {
+      _mint(to, amount);
+    }
+
+    function burn(address from, uint amount) public {
+      _burn(from, amount);
+    }
+
+}
