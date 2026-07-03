aa5102751ac5a8c474282bce5e1b79c295c7aac0
deoidh <samuel.jj.gosling@gmail.com>
2026-07-03 09:34:46 +0100

    introduce bootstrapped voting module extension

---
+++ src/modules/strategies/extensions/BootstrappedVotingStrategy.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+abstract contract BootstrappedVotingStrategy {
+
+    /// @notice Tenure mapping for accounts, acting as a basis 
+    mapping(address => uint) public seeds;
+
+    /**
+      * @notice Get seeded effective time for an account  
+      * @param owner The address to query for
+      * @return Baseline effective time 
+    **/
+    function seededEffectiveTime(address owner) public view returns (uint) {
+        return seeds[owner];
+    }
+
+    /**
+      * @notice Returns the future projected voting power of a given account
+      * @param owner The address to query for
+      * @param timestamp The future time to query the voting power at
+      * @return The future voting power of the account 
+    **/
+    function predict(address owner, uint timestamp) public view virtual returns (uint);
+
+}
