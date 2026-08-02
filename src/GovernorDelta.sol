aecc7c87d348523297c8d7c42e85a6fe424f2b55
deoidh <samuel.jj.gosling@gmail.com>
2026-08-02 15:18:15 +0100

    enforce guard hook on relay

--- src/GovernorDelta.sol
+++ src/GovernorDelta.sol
@@ -422,7 +422,7 @@ contract GovernorDelta is GovernorStorageV3 {
       * @param data The calldata to forward to the target
       * @return The raw returndata from the target call
     **/
-    function relay(address target, uint value, bytes calldata data) external payable returns (bytes memory) {
+    function relay(uint proposalId, address target, uint value, bytes calldata data) guarded(address(this), proposalId) external payable returns (bytes memory) {
         require(msg.sender == address(timelock), "GovernorDelta::relay: timelock only");
         (bool success, bytes memory returnData) = target.call{value: value}(data);
         require(success, "GovernorDelta::relay: call reverted");
